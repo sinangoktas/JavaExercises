ed18a281addbab02f7e1df1481b97cb3082e862c
sinangoktas <sinan.goktas@gmail.com>
2018-02-28 02:42:44 +0000

    rebuild

---
+++ src/com/example/idea/Interfaces/java8_note_3_2/Person.java
@@ -0,0 +1,12 @@
+package com.example.idea.Interfaces.java8_note_3_2;
+
+/**
+   This interface provides a default method name, just like Named.
+*/
+public interface Person
+{
+   default String name() { return firstName() + " " + lastName(); }
+   
+   String firstName();
+   String lastName();
+}
--- src/com/example/idea/Interfaces/javaEarlyObjs/java8_note_3_2/Named.java
+++
@@ -1,9 +0,0 @@
-package com.example.idea.Interfaces.javaEarlyObjs.java8_note_3_2;
-
-/**
-   This interface provides a default method name, just like Person.
-*/
-public interface Named
-{
-   default String name() { return "(NONE)"; }
-}
