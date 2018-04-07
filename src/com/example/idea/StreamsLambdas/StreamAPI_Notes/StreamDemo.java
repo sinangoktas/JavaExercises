b324e0c7a9f497962f7a31f85c3e3a4886da28f2
sinangoktas <sinan.goktas@gmail.com>
2018-04-07 03:23:38 +0100

    stream&lambdas update

--- src/com/example/idea/StreamsLambdas/StreamAPI_Notes/StreamDemo.java
+++ src/com/example/idea/StreamsLambdas/StreamAPI_Notes/StreamDemo.java
@@ -28,5 +28,38 @@ public class StreamDemo {
         if(minVal.isPresent())
             System.out.println("Minimum value: " + minVal.get());
 
+
+        //Must obtain a new stream because previous call to min()
+        //is a terminal operation that consumed the stream.
+
+        myStream = myList.stream();
+        Optional<Integer> maxValue = myStream.max(Integer::compare);
+        if(maxValue.isPresent())
+            System.out.println("Maximum value: " + maxValue.get());
+
+        // Sort the stream by use of sorted().
+        Stream<Integer> sortedStream = myList.stream().sorted();
+
+        // Display the sorted stream by the use of forEach().
+        System.out.print("Sorted stream: ");
+        sortedStream.forEach((n) -> System.out.print(n + " "));
+        System.out.println();
+
+        // Display only the odd values by use of filter().
+        Stream<Integer> oddVals = myList.stream().sorted()
+                .filter((n) -> (n % 2) == 1);
+        System.out.print("Odd values: ");
+        oddVals.forEach((n) -> System.out.print(n + " "));
+        System.out.println();
+
+        // Display only the odd values that are greater than 5. Notice that
+        // two filter operations are pipelined.
+        oddVals = myList.stream().filter((n) -> (n % 2) == 1)
+                .filter((n) -> n > 5);
+        System.out.print("Odd values greater than 5: ");
+        oddVals.forEach((n) -> System.out.print(n + " "));
+        System.out.println();
+
+
     }
 }
