e8e8d860a25166cc517cb8d01b020b2b6f6ea548
sinangoktas <sinan.goktas@gmail.com>
2018-04-23 22:40:18 +0100

    stream api

---
+++ src/com/example/idea/JDK8_MOOC/theStreamAPI/Student.java
@@ -0,0 +1,20 @@
+package com.example.idea.JDK8_MOOC.theStreamAPI;
+
+public class Student {
+
+    int gradYear;
+    double score;
+
+    public Student(int gradYear, double score) {
+        this.gradYear = gradYear;
+        this.score = score;
+    }
+
+    public int getGradYear() {
+        return gradYear;
+    }
+
+    public double getScore() {
+        return score;
+    }
+}
