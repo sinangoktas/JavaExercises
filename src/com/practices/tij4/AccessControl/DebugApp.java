cca6b428fcb009f7064046892f539d6be5d2acc8
Sinan Goktas <sinan.goktas@gmail.com>
2019-03-27 23:55:26 +0000

    access control

--- src/com/practices/tij4/AccessControl/DebugApp.java
+++ src/com/practices/tij4/AccessControl/DebugApp.java
@@ -1,5 +1,6 @@
 package com.practices.tij4.AccessControl;
 
+//import static com.practices.tij4.AccessControl.debugoff.Debug.debug;
 import static com.practices.tij4.AccessControl.debug.Debug.debug;
 
 public class DebugApp {
---
+++ src/com/practices/tij4/AccessControl/debugoff/Debug.java
@@ -0,0 +1,8 @@
+package com.practices.tij4.AccessControl.debugoff;
+
+public class Debug {
+
+    public static void debug(String s) {
+
+    }
+}
