f774901b7db464a41003fe0348a7bf3c3b1a4a5f
Sinan Goktas <sinan.goktas@gmail.com>
2019-03-27 19:29:55 +0000

    update

---
+++ src/com/practices/tij4/Initialisation/NoConstructor.java
@@ -0,0 +1,10 @@
+package com.practices.tij4.Initialisation;
+
+public class NoConstructor {
+
+    // default constructor is automatically initialised
+
+    public static void main(String[] args) {
+        System.out.println(new NoConstructor());
+    }
+}
