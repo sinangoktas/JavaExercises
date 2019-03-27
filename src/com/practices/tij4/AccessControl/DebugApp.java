ac86867a93995f2201b9cdd6fa292d5b615209de
Sinan Goktas <sinan.goktas@gmail.com>
2019-03-27 23:31:20 +0000

    update

---
+++ src/com/practices/tij4/AccessControl/DebugApp.java
@@ -0,0 +1,11 @@
+package com.practices.tij4.AccessControl;
+
+import static com.practices.tij4.AccessControl.debug.Debug.debug;
+
+public class DebugApp {
+    public static void main(String[] args) {
+       debug("Debugged.....");
+    }
+
+
+}
