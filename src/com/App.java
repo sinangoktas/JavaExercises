121b1fbc3f6d8b7145f5154622b85fcf5581a31a
sinagoktas <sinan.goktas@digital.homeoffice.gov.uk>
2019-05-08 15:42:02 +0100

    update

--- src/com/App.java
+++ src/com/App.java
@@ -1,19 +1,8 @@
 package com;
 
-import java.util.Random;
-
 public class App {
 
     public static void main(String[] args) {
-
-        int[] accessControl = new int[9];
-        for (int i = 0; i < accessControl.length; i++) {
-            accessControl[i] = i + 1;
-        }
-
-        int random = new Random().nextInt(accessControl.length);
-
-        System.out.println(accessControl[random]);
 
 
       }
---
+++ src/com/practices/tij4/ReusingClasses/Root.java
@@ -0,0 +1,16 @@
+package com.practices.tij4.ReusingClasses;
+
+public class Root {
+}
+
+class Component1 {
+
+}
+
+class Component2 {
+
+}
+
+class Component3 {
+
+}
