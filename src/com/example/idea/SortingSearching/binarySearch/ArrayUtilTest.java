f94c7fb9e1b881d30eb57bb98097bdf0eecb4c52
Sinan Goktas <sinan.goktas@gmail.com>
2019-09-25 17:19:11 +0100

    src/com

---
+++ src/com/example/idea/SortingSearching/binarySearch/ArrayUtilTest.java
@@ -0,0 +1,35 @@
+package com.example.idea.SortingSearching.binarySearch;
+
+import org.junit.Assert;
+import org.junit.BeforeClass;
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+
+class ArrayUtilTest {
+
+    ArrayUtil arrayUtil;
+    int[] intArr;
+
+    @BeforeClass
+    void setUp() {
+        arrayUtil = new ArrayUtil();
+    }
+
+    @Test
+    void randomIntArrayTest() {
+        int[] arr = arrayUtil.randomIntArray(5, 5);
+        Assert.assertEquals(arr.length, 5);
+
+    }
+
+    @Test
+    void swapTest() {
+        intArr = new int[] {1,2,3,4,5};
+        arrayUtil = new ArrayUtil();
+        arrayUtil.swap(intArr, 0, 4);
+        Assert.assertEquals(intArr[0], 5);
+        Assert.assertEquals(intArr[4], 1);
+
+    }
+}
