2fe3349999dce7043182f1d8b799f9ed1cb9ab40
Sinan Goktas <sinan.goktas@gmail.com>
2021-05-17 00:20:02 +0100

    update

--- src/com/main/OracleExamples/Collections/FindDups.java
+++ src/com/main/OracleExamples/Collections/FindDups.java
@@ -1,6 +1,8 @@
 package com.main.OracleExamples.Collections;
 
+import java.util.ArrayList;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
 public class FindDups {
@@ -8,10 +10,17 @@ public class FindDups {
     public static void main(String[] args) {
 
         String[] arr = new String[] {"sinan", "ali", "devrim", "ali"};
-
+        List<String> dups = new ArrayList<>();
         Set<String> s = new HashSet<>();
+
         for (String a : arr)
-            s.add(a);
+            if (s.contains(a)) {
+                dups.add(a);
+        } else {
+                s.add(a);
+            }
+
+        System.out.println(dups.size() + " duplicate words: " + dups);
         System.out.println(s.size() + " distinct words: " + s);
 
         //  modify it to use a SortedSet instead of a Set.
---
+++ src/com/main/OracleExamples/LambdaNdStream/AgregateOperations.java
@@ -0,0 +1,8 @@
+package com.main.OracleExamples.LambdaNdStream;
+
+public class AgregateOperations {
+
+    // https://docs.oracle.com/javase/tutorial/collections/streams/index.html
+
+
+}
